40412aaa1bec362ddf881b778b0c389234539fef
Veljko28 <charlibear28@gmail.com>
2021-06-10 13:01:23 +0200

    fetching users from api and stroing them in store

--- src/redux/user/userReducer.ts
+++ src/redux/user/userReducer.ts
@@ -1,15 +1,24 @@
 import { Action } from '../tsTypes';
-import { LOGIN_USER } from './userTypes';
+import { API_USERS, API_USERS_FAILURE, API_USERS_SUCCESS, LOGIN_USER } from './userTypes';
 
 
 const initState = {
-  userDetails: {}
+  userDetails: {},
+  apiUsers: [],
+  errors: "",
+  loading: false
 }
 
 const userReducer = (state = initState, action : Action ) => {
   switch (action.type) {
     case LOGIN_USER:
       return {...state, userDetails: action.payload};
+    case API_USERS:
+      return {...state, loading: true } 
+    case API_USERS_SUCCESS: 
+       return {...state, loading: false, apiUsers: action.payload}
+    case API_USERS_FAILURE
+    :  return {...state, loading: false, errors: action.payload}
     default: 
       return state;
   }
--- src/redux/user/userTypes.ts
+++ src/redux/user/userTypes.ts
@@ -1,7 +1,45 @@
+import axios from "axios";
+
 export const LOGIN_USER = "LOGIN_USER";
+export const API_USERS = "API_USERS";
+export const API_USERS_SUCCESS = "API_USERS_SUCCESS";
+export const API_USERS_FAILURE = "API_USERS_FAILURE";
+
+
+
 
 
 export const loginUser = (user: {name: string, password: string}) => ({
   type: LOGIN_USER,
   payload: user
 });
+
+export const apiUsers = () => ({
+  type: API_USERS,
+})
+
+
+export const apiUsersSuccess = (users: any) => ({
+  type: API_USERS_SUCCESS,
+  payload: users
+})
+
+export const apiUsersFailure = (errors: any) => ({
+  type: API_USERS_FAILURE,
+  payload: errors
+})
+
+
+export const getApiUsers = () => (dispatch: any) => {
+  dispatch(apiUsers());
+  axios.get("https://jsonplaceholder.typicode.com/users")
+  .then(res => {
+    const users = res.data;
+    console.log(users);
+    dispatch(apiUsersSuccess(users))
+  })
+  .catch(err => {
+    dispatch(apiUsersSuccess(err.message))
+  });
+  
+}
